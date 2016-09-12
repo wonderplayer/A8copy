cbf72fe8aa2df48dfe777a7cccdf10eb9bc0787e
Rolands Strakis <rolands.strakis@accenture.com>
2016-09-12 18:33:42 +0300

    Finished?

--- A8/A8.cpp
+++ A8/A8.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include "A8.h"
 
+using namespace std;
 
 int main()
 {
+	int firstNumber;
+	int secondNumber;
+	string answer = "";
+
+	cout << "Ievadiet pirmo skaitili: ";
+	cin >> firstNumber;
+	cout << "Ievadiet otro skaitli: ";
+	cin >> secondNumber;
+
+	answer = compareNumbers(firstNumber, secondNumber);
+
+	cout << answer;
+
     return 0;
 }
+
+string compareNumbers(int first, int second)
+{
+	if (first % second == 0 || second % first == 0) {
+		return "Skaitli ir savstarpeji pirmskaitli.";
+	}
+
+	return "Skaitli nav savstarpeji pirmskaitli.";
+}
+
 
---
+++ A8/A8.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int main();
+std::string compareNumbers(int first, int second);
